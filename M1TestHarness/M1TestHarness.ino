c6144a60dad44c239feeb647a2af2b1dc6f107ae
Marcel Erz <info@retrostack.org>
2025-08-14 21:46:52 -0700

    Support Arduino IDE

---
+++ M1TestHarness/M1TestHarness.ino
@@ -0,0 +1,74 @@
+
+#include <Arduino.h>
+
+#include "globals.h"
+#include "screens/WelcomeConsole.h"
+
+// First, tell the system which display you have
+// For ST7789 240x320 displays (most common, landscape becomes 320x240)
+#include <Display_ST7789_320x240.h>
+Display_ST7789_320x240 displayProvider;
+
+// For ST7789 240x240 square displays
+// #include <Display_ST7789_240x240.h>
+// Display_ST7789_240x240 displayProvider;
+
+// For ST7789 320x170 wide displays (landscape)
+// #include <Display_ST7789_320x170.h>
+// Display_ST7789_320x170 displayProvider;
+
+// For ST7789 320x240 alternative displays (landscape)
+// #include <Display_ST7789_320x240.h>
+// Display_ST7789_320x240 displayProvider;
+
+// For smaller ST7735 128x160 displays
+// #include <Display_ST7735.h>
+// Display_ST7735 displayProvider;
+
+// For large ST7796 320x480 displays (landscape becomes 480x320)
+// #include <Display_ST7796.h>
+// Display_ST7796 displayProvider;
+
+// For parallel ILI9325 240x320 displays (landscape becomes 320x240)
+// #include <Display_ILI9325.h>
+// Display_ILI9325 displayProvider;
+
+// For ILI9341 240x320 displays (landscape becomes 320x240)
+// #include <Display_ILI9341.h>
+// Display_ILI9341 displayProvider;
+
+// For large HX8357 320x480 displays
+// #include <Display_HX8357.h>
+// Display_HX8357 displayProvider;
+
+// For monochrome SSD1306 OLED displays (128x64)
+// #include <Display_SSD1306.h>
+// Display_SSD1306 displayProvider;
+
+// For monochrome SH1106 OLED displays (128x64)
+// #include <Display_SH1106.h>
+// Display_SH1106 displayProvider;
+
+void setup() {
+  Serial.begin(500000);
+
+  Model1.begin(2);  // Setup Timer 2
+  M1Shield.begin(displayProvider);
+
+  // Enable joystick input for navigation
+  // Uncomment to allow joystick control of menus and screens
+  //   M1Shield.activateJoystick();
+
+  Globals.logger.info(F("=== TRS-80 Model 1 Testharness ==="));
+
+  // Start with the welcome console
+  M1Shield.setScreen(new WelcomeConsole());
+}
+
+ISR(TIMER2_COMPA_vect) {
+  Model1.nextUpdate();
+}
+
+void loop() {
+  M1Shield.loop();
+}
