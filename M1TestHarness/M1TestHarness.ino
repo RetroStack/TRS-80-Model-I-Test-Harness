bc91b10419050c088e847d3cb63d4d8fd817e24b
Marcel Erz <info@retrostack.org>
2025-08-16 21:21:08 -0700

    Add logo

--- M1TestHarness/M1TestHarness.ino
+++ M1TestHarness/M1TestHarness.ino
@@ -4,6 +4,7 @@
 #include "./globals.h"
 #include "./screens/WelcomeConsole.h"
 // #include "./screens/MainMenu.h"
+#include "retro_stack_240x240.h"
 
 // First, tell the system which display you have
 // For ST7789 240x320 displays (most common, landscape becomes 320x240)
@@ -50,6 +51,19 @@ Display_ST7789_320x240 displayProvider;
 // #include <Display_SH1106.h>
 // Display_SH1106 displayProvider;
 
+void drawLogo(Adafruit_GFX &gfx, uint16_t width, uint16_t height) {
+  gfx.fillScreen(0x000F);  // background similar to your logo’s bg
+
+  // Centered position for 240x240 on a 320x240 screen
+  const int16_t x = (width - RETRO_STACK_WIDTH) / 2;
+  const int16_t y = (height - RETRO_STACK_HEIGHT) / 2;
+
+  // Draw the PROGMEM RGB565 bitmap
+  // Many Adafruit_GFX drivers support reading RGB565 from PROGMEM with drawRGBBitmap.
+  gfx.drawRGBBitmap(x, y, (const uint16_t *)retro_stack_240x240, RETRO_STACK_WIDTH,
+                    RETRO_STACK_HEIGHT);
+}
+
 void setup() {
   Serial.begin(115200);
 
@@ -61,6 +75,10 @@ void setup() {
   //   M1Shield.activateJoystick();
 
   Globals.logger.infoF(F("=== TRS-80 Model 1 Testharness ==="));
+
+  // Show logo
+  drawLogo(displayProvider.getGFX(), displayProvider.width(), displayProvider.height());
+  delay(2000);
 
   // Start with the welcome console
   M1Shield.setScreen(new WelcomeConsole());
